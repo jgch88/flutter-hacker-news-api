01edf51a2310d97af6158eb0744dbe4b9555e767
jgch88 <radium.laboratories@gmail.com>
2019-01-22 00:00:18 +0800

    More on the Repository

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -3,17 +3,29 @@ import 'news_api_provider.dart';
 import 'news_db_provider.dart';
 import 'dart:async';
 
+// governs access to our providers
 class Repository {
   NewsDbProvider dbProvider = NewsDbProvider();
   NewsApiProvider apiProvider = NewsApiProvider();
   // remember to init() dbProvider! constructor not sufficient
 
   fetchTopIds() {
-
+    return apiProvider.fetchTopIds(); // not caching top ids, just one network call
   }
 
-  fetchItem() {
-    
+  fetchItem(int id) async {
+    // check dbProvider first
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+
+    // didn't find it in db, use apiProvider
+    item = await apiProvider.fetchItem(id);
+    // store it for future use
+    dbProvider.addItem(item);
+
+    return item;
   }
 
 }

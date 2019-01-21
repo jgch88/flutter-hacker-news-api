276b8907b27dff410e518e1ab60542b3e09dc48b
jgch88 <radium.laboratories@gmail.com>
2019-01-21 22:56:17 +0800

    A Few Imports

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -1,2 +1,8 @@
 // tests MUST end in _test.dart for the test runner to find it
+import 'package:news/src/resources/news_api_provider.dart';
+// can't do relative reference if outside lib library
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
 

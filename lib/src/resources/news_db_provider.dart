f4afd1682cfb018f9211648a01b9199b9cfabfe6
jgch88 <radium.laboratories@gmail.com>
2019-01-21 23:30:50 +0800

    Creating a DB Connection

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -8,10 +8,22 @@ import '../models/item_model.dart';
 class NewsDbProvider {
   Database db; // link to sqflite database
 
-
   // can't use this logic in constructors because you can't do async code
   // in constructors
   init() async {
-    
+    // returns a directory where you can safely store stuff permanently
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    // actual path
+    final path = join(documentsDirectory.path, "items.db");
+
+    // if user is opening the app for the first time, they do not have a db
+    // openDatabase tries to find an existing db OR create a new db
+    db = await openDatabase(
+      path,
+      version: 1, // schema versions for migrations
+      onCreate: (Database newDb, int version) { // only called if db is being created
+
+      }
+    );
   }
 }

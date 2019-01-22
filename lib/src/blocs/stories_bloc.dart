221378b72e6023663cc069f4bdd222d32e54add9
jgch88 <radium.laboratories@gmail.com>
2019-01-22 12:24:22 +0800

    StreamBuilder Setup

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -8,12 +8,17 @@ class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>(); // similar to StreamController
   final _items = BehaviorSubject<int>(); // this sink must be exposed outside, other screens need item info
+  Observable<Map<int, Future<ItemModel>>> items;
 
   // Getters to streams
   Observable<List<int>> get topIds => _topIds.stream;
 
   // Getters to sinks
   Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer()); // publicly expose the transformed stream, that widgets can observe
+  }
 
   // hide the sink, we don't want it publicly available
   // since the only thing that needs access to it is the repository
---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+import 'dart:async';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId; // passed in by parent
+
+  NewsListTile({this.itemId});
+
+  Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot){
+        if (!snapshot.hasData){
+          return Text('Stream still loading');
+        }
+      }
+    );
+  }
+}

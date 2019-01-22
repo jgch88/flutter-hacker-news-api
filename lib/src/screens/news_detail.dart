248e689798a985a94971571ee6fc328baacc9502
jgch88 <radium.laboratories@gmail.com>
2019-01-22 17:07:20 +0800

    A Scaffold in NewsDetail

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
   Widget build(context) {
-    return Text("I'm a news detail");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: Text("I'm a details screen"),
+    );
   }
 }

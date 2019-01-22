271ab4059e34fa4effca808415dff5cf179f1831
jgch88 <radium.laboratories@gmail.com>
2019-01-22 11:32:09 +0800

    Bloc Design

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,14 @@
-class StoriesBloc {
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
 
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>(); // similar to StreamController
+
+  // Getters to streams
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
 }

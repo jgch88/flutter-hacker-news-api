fa4ada9d04c32772eeb547244bb0cea337b16efa
jgch88 <radium.laboratories@gmail.com>
2019-01-22 11:18:06 +0800

    Cleanup for BLOC implementation

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -7,35 +6,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
-    );
-  }
-
-  // demo on how ListView.builder works
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-
-            // ListView.builder adapts to the item Heights (visible items)
-            return Container(
-              height: 80.0,
-              child: snapshot.hasData ? Text("I'm visible $index")
-                  : Text("I haven't been fetched yet"),
-            );
-          }
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-        () => 'hi',
+      body: Text('Show List'),
     );
   }
 }

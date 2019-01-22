e941f8f75831ed9ae92c716578d8ffdcf9931205
jgch88 <radium.laboratories@gmail.com>
2019-01-22 11:38:28 +0800

    Wiring up the Stories Provider

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+    // crawls up to tree to get reference
+    // to the bloc
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),

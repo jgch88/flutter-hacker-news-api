d6793a585dc1734624c6f7a82225e8733715e91b
jgch88 <radium.laboratories@gmail.com>
2019-01-22 11:27:34 +0800

    The Stories Provider

---
+++ lib/src/blocs/stories_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'stories_bloc.dart';
+
+// InheritedWidget allows us to reach up the context tree
+// and get an instance of the StoriesProvider
+// so that it can get access to the Stories BLOC
+class StoriesProvider extends InheritedWidget {
+  final StoriesBloc bloc;
+
+  StoriesProvider({Key key, Widget child})
+    : bloc = StoriesBloc(),
+      super(key: key, child: child);
+
+  bool updateShouldNotify(_) => true;
+
+  static StoriesBloc of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(StoriesProvider)
+            as StoriesProvider)
+            .bloc;
+  }
+}

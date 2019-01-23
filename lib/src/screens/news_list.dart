affa4d5f513eb058da177516da5f64665093b9f3
jgch88 <radium.laboratories@gmail.com>
2019-01-23 14:03:26 +0800

    BLOC Stream concepts

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -18,7 +18,7 @@ class NewsList extends StatelessWidget {
   }
 
   /*
-  StreamBuilder only needs to consume from a stream.
+  Here, the StreamBuilder only needs to consume from a stream (RxDart's PublishSubject).
   It doesn't care about the sink. The StoriesSink was kickstarted
   in App.Routes() via storiesBloc.fetchTopIds().
 
@@ -35,10 +35,23 @@ class NewsList extends StatelessWidget {
           );
         }
 
+        /*
+        Refresh is actually using the RefreshIndicator, and
+        implements the bloc refresh logic within onRefresh().
+         */
         return Refresh(
           child: ListView.builder(
             itemCount: snapshot.data.length,
             itemBuilder: (context, int index) {
+              /*
+              Here the individual item stream (RxDart's PublishSubject)
+              exposes both the sink and stream.
+
+              We're using the individual item sink here to pass it itemId events.
+
+              The individual item's stream is consumed not here, but
+              in NewsListTile's StreamBuilder.
+              */
               bloc.fetchItem(snapshot.data[index]);
 
               return NewsListTile(itemId: snapshot.data[index]);

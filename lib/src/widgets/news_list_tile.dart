6fe28a15f3dd38a702bb4bfb00ea176289a87174
jgch88 <radium.laboratories@gmail.com>
2019-01-23 14:19:38 +0800

    BLOC Stream concepts

--- lib/src/widgets/news_list_tile.dart
+++ lib/src/widgets/news_list_tile.dart
@@ -19,6 +19,18 @@ class NewsListTile extends StatelessWidget {
           return LoadingContainer();
         }
 
+        // this is an individual tile, so render an individual Future's
+        // snapshot accordingly. but notice we're still accessing
+        // the storiesBloc's items snapshot
+
+        // within each item (snapshot.data[itemId]) is a future, which also has a
+        // snapshot depending on its state
+
+        // https://docs.flutter.io/flutter/widgets/AsyncSnapshot-class.html
+        // We get AsyncSnapshots for free when using StreamBuilders/FutureBuilders,
+        // this helps us code the conditional widget building based on
+        // stream/future snapshots, without having to worry about
+        // the mechanism of these async programming concepts.
         return FutureBuilder(
           future: snapshot.data[itemId],
           builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
